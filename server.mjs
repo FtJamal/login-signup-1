b8df5c656d535b28aa59dbd4cbf7863b5b81f4aa
farya <ftjamal2017@gmail.com>
2022-08-20 19:08:48 +0500

    mongodb install

--- server.mjs
+++ server.mjs
@@ -1,6 +1,37 @@
 import express from 'express'
 import cors from "cors"
 import { nanoid } from 'nanoid'
+import mongoose from 'mongoose';
+
+// mongoose.connect('mongodb+srv://abc:abc@cluster0.uhv9f8j.mongodb.net/?retryWrites=true&w=majority');
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+let dbURI = 'mongodb+srv://abc:abc@cluster0.uhv9f8j.mongodb.net/socialMediaDB?retryWrites=true&w=majority';
+mongoose.connect(dbURI);
+
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+mongoose.connection.on('connected', function() {//connected
+    console.log("Mongoose is connected");
+});
+
+mongoose.connection.on('disconnected', function() {//disconnected
+    console.log("Mongoose is disconnected");
+    process.exit(1);
+});
+
+mongoose.connection.on('error', function(err) {//any error
+    console.log('Mongoose connection error: ', err);
+    process.exit(1);
+});
+
+process.on('SIGINT', function() {/////this function will run jst before app is closing
+    console.log("app is terminating");
+    mongoose.connection.close(function() {
+        console.log('Mongoose default connection closed');
+        process.exit(0);
+    });
+});
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
 
 const app = express();
 app.use(express.json());
